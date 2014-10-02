77d0f57ace219d10d6d4825ff3ee25e4a26aea0a
Youyang Gu <youyanggu@gmail.com>
2014-10-01 23:08:35 -0400

    added comprehensive tests

--- tests/semantics/parse_test.sh
+++ tests/semantics/parse_test.sh
@@ -6,14 +6,14 @@ runparser() {
 
 fail=0
 
-for file in `dirname $0`/illegal/*; do
+for file in `dirname $0`/illegal/*.dcf; do
   if ! runparser $file; then
     echo "Legal file $file failed to parse.";
     fail=1
   fi
 done
 
-for file in `dirname $0`/legal/*; do
+for file in `dirname $0`/legal/*.dcf; do
   if ! runparser $file; then
     echo "Legal file $file failed to parse.";
     fail=1
