345a0ab9decfeedafd059b7c4bf6c3bfe3c0f181
Desai Chen <desaicwtf@gmail.com>
2014-10-09 12:15:54 -0400

    codegen skeleton

--- src/edu/mit/compilers/codegen/Instruction.java
+++ src/edu/mit/compilers/codegen/Instruction.java
@@ -6,4 +6,15 @@ public class Instruction {
 	public String cmd;
 	public ArrayList<String> args;
 	public String label;
+	public Instruction(String c, String a1){
+		cmd = c;
+		args = new ArrayList<String> ();
+		args.add(a1);
+	}
+	public Instruction(String c, String a1, String a2){
+		cmd = c;
+		args = new ArrayList<String> ();
+		args.add(a1);
+		args.add(a2);
+	}
 }
