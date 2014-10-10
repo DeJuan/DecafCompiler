2ff36418e44fb99f7325964abb77533ed8157be0
Desai Chen <desaicwtf@gmail.com>
2014-10-10 11:38:10 -0400

    experiment with array

--- src/edu/mit/compilers/codegen/CodegenContext.java
+++ src/edu/mit/compilers/codegen/CodegenContext.java
@@ -6,17 +6,37 @@ import Descriptors.Descriptor;
 import edu.mit.compilers.ir.SymbolTable;
 
 public class CodegenContext {
+	
 	/**@brief flat list of assembly code.
 	 * Append to this list as appropriate.
 	 */
 	public ArrayList<Instruction> ins;
+	
+	/**@brief symbol table for locations of variables*/
 	public SymbolTable<Descriptor> symbol;
+	
+	/**@brief size of local variables in a function*/
+	long localvarSize;
+	
 	public MemLocation rsp;
+	
+	public CodegenContext(){
+		
+	}
+	
+	public void incScope(){
+		symbol.incScope();
+	}
+	
+	public void decScope(){
+		symbol.decScope();
+	}
+
 	/**@brief print instructions to a file.
 	 * 
 	 * @param ps target of print. System.out or an output file stream.
 	 */
-	public void printInstructions(PrintStream ps){
+	public void generateInstructions(PrintStream ps){
 		
 	}
 	
@@ -28,7 +48,7 @@ public class CodegenContext {
 		
 	}
 	
-	public void pop(MemLocation loc){
-		
+	public MemLocation pop(){
+		return null;
 	}
 }
--- src/edu/mit/compilers/codegen/MemLocation.java
+++ src/edu/mit/compilers/codegen/MemLocation.java
@@ -1,18 +1,41 @@
 package edu.mit.compilers.codegen;
 
 abstract public class MemLocation {
-	abstract public String getLocation();
 	
-	public static class StackLocation{
+	public enum LocType{
+		STACK_LOC, LABEL_LOC, LITERAL_LOC, ARRAY_LOC,
+		REG_LOC
+	};
+	
+	public enum Regs{
+		RAX, RBX, RCX, RDX, RSP, RBP, RSI, RDI,R8,R9,R10,R11
+	};
+	
+	abstract public LocType getType(); 
+	
+	public class StackLocation{
 		/**@brief with respect to rbp
 		 */
 		public long offset;
+		public String toString(){
+			return offset+"(%rbp)";
+		}
+		
+		public LocType getType(){
+			return LocType.STACK_LOC;
+		}
 	}
 	
-	public static class GlobalLocation{
+	public class LabelLocation{
 		/**@brief label for global variables and arrays
 		 */
 		public String label;
+		public String toString(){
+			return label;
+		}
+		public LocType getType(){
+			return LocType.LABEL_LOC;
+		}
 	}
 	
 	/**@brief for int literals
@@ -20,7 +43,60 @@ abstract public class MemLocation {
 	 * @author desaic
 	 *
 	 */
-	public static class LiteralLocation{
+	public class LiteralLocation{
 		public long val;
+		public String toString(){
+			return "$"+val;
+		}
+		public LocType getType(){
+			return LocType.LITERAL_LOC;
+		}
+	}
+	
+	public class RegLocation{
+		public Regs reg;
+		public String toString(){
+			return "%"+reg;
+		}
+		public LocType getType(){
+			return LocType.REG_LOC;
+		}
+	}
+	
+	public class ArrayLocation{
+		/**@brief there are 4 combinations of acceptable 
+		 * array addressing:
+		 * label + literal  
+		 * label + register
+		 * stack + literal
+		 * stack + register
+		 */
+		public MemLocation array;
+		public MemLocation offset;
+		public String toString(){
+			switch(array.getType()){
+			case LABEL_LOC:
+				if(offset.getType()==LocType.REG_LOC){
+					
+				}else if(offset.getType()==LocType.LITERAL_LOC){
+					
+				}
+				break;
+			case STACK_LOC:
+				if(offset.getType()==LocType.REG_LOC){
+					
+				}else if(offset.getType()==LocType.LITERAL_LOC){
+					
+				}
+				break;
+			default:
+				break;
+			}
+			return null;
+		}
+		
+		public LocType getType(){
+			return LocType.ARRAY_LOC;
+		}
 	}
 }
