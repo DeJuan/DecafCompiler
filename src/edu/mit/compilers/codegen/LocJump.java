c3962d954248307b087bcccc697a8df59c6639dd
Desai Chen <desaicwtf@gmail.com>
2014-10-11 14:08:39 -0400

    global field

--- src/edu/mit/compilers/codegen/LocJump.java
+++ src/edu/mit/compilers/codegen/LocJump.java
@@ -1,5 +1,8 @@
 package edu.mit.compilers.codegen;
 
+/**@brief Location represented by a jump label.
+ * can be destination of jump instructions and also function names.
+ */
 public class LocJump extends LocationMem {
 	public String label;
 	public LocJump(String l){
