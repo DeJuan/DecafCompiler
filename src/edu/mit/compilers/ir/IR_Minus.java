a6702d8cf52a737079e7968b9079923452a8c419
Youyang Gu <youyanggu@gmail.com>
2014-10-14 12:23:59 -0400

    renamed IR_Negate --> IR_Minus; added handling in Codegen

--- src/edu/mit/compilers/ir/IR_Minus.java
+++ src/edu/mit/compilers/ir/IR_Minus.java
@@ -1,12 +1,12 @@
 package edu.mit.compilers.ir;
 
-public class IR_Negate extends IR_Node {
+public class IR_Minus extends IR_Node {
 	private IR_Node expr;
 	
 	public Ops getOp(){
 		return Ops.MINUS;
 	}
-	public IR_Negate(IR_Node e){
+	public IR_Minus(IR_Node e){
 		expr = e;
 	}
 	public IR_Node getExpr(){
