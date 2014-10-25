217019637441b5715b1503ad0ea9ddd60c5d088a
dejuan <dejuan@mit.edu>
2014-10-25 13:36:56 -0400

    Added return constructor and private Expression value to END to handle return values.

--- src/edu/mit/compilers/codegen/END.java
+++ src/edu/mit/compilers/codegen/END.java
@@ -3,9 +3,12 @@ package edu.mit.compilers.codegen;
 import java.util.List;
 
 public class END extends FlowNode {
-
+	Expression returnStatement = null;
 	public END(){}
 	
+	public END(Expression returnVal){
+		this.returnStatement = returnVal;
+	}
 	@Override
 	public NodeType getType() {
 		return NodeType.END;
@@ -17,6 +20,9 @@ public class END extends FlowNode {
 		return null;
 	}
 
+	public Expression getReturnStatement(){
+		return returnStatement;
+	}
 	@Override
 	public List<FlowNode> getChildren() {
 		// TODO Auto-generated method stub
