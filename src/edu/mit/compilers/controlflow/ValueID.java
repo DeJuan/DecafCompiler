5c0d1e27bb31c8038ea6f7082afd1e6f056609c4
dejuan <dejuan@mit.edu>
2014-11-02 01:40:36 -0500

    Forgot to commit ValueID.java.

---
+++ src/edu/mit/compilers/controlflow/ValueID.java
@@ -0,0 +1,14 @@
+package edu.mit.compilers.controlflow;
+
+public class ValueID {
+	static int nextID = 0;
+    private final String ID;
+    
+    public ValueID(){
+        ID = "v" + nextID++;
+    }
+    
+    public String getString(){
+        return ID;
+    }
+}
