4b080d94a6baa03cdb731c293465b02049c5984d
Maddie Mott <mmott@mit.edu>
2014-11-05 04:26:21 -0500

    forgot new location class

---
+++ src/edu/mit/compilers/codegen/LocRelStack.java
@@ -0,0 +1,35 @@
+package edu.mit.compilers.codegen;
+
+
+public class LocRelStack extends LocationMem {
+
+    /**@brief with respect to rbp. Measured in bytes.
+     */
+    public long offset;
+    public LocRelStack(){
+        offset = 0;
+    }
+    public LocRelStack(long o){
+        offset = o;
+    }
+    public String toString(){
+        return offset+"(%rsp)";
+    }
+    
+    public LocType getType(){
+        return LocType.STACK_LOC;
+    }
+    
+    public long getValue(){
+        return offset;
+    }
+    
+    public void setValue(long v){
+        offset = v;
+    }
+    
+    public LocRelStack clone(){
+        return new LocRelStack(offset);
+    }
+
+}
